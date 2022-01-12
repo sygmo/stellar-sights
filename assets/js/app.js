9731f8e582d448a5869f3f8590ab1e971fc22e18
Steven Schmidt <stevenschmidtaustex@gmail.com>
2022-01-12 14:09:28 -0600

    Alligning with Dev

--- assets/js/app.js
+++ assets/js/app.js
@@ -82,12 +82,135 @@ function getAstro() {
 
 
 
-
-
-
 getAstro()
 
+//** start of get weather function
+var weatherAPIKEY =  '27bbc4e6b84a47d1b13160933221101' ;
+var zipcodeNUM = '78634' ;
 
+// Get weather function 
+// will grab weather information from API 
+// data responds in current, forecast , location
+
+console.log("start Get Weather funtion");
+
+function getWeather () {
+    fetch('http://api.weatherapi.com/v1/forecast.json?key=' + weatherAPIKEY + '&q=' + zipcodeNUM + '&days=3')
+    
+      .then(function(response){
+        if (response.ok){
+
+          console.log("response ");
+          console.log(response);
+
+          response.json()
+            .then(function(data) {
+
+              console.log("data ");
+              console.log(data);
+
+              console.log("data current conditions ");
+              console.log(data.current.condition);
+              console.log("day 1 ");
+              console.log(data.forecast.forecastday[0]);
+              console.log("Moon Phase " + data.forecast.forecastday[0].astro.moon_phase);
+              console.log("Moonrise "+ data.forecast.forecastday[0].astro.moonrise);
+              console.log("Moonset " + data.forecast.forecastday[0].astro.moonset);
+              console.log("Sunrise " + data.forecast.forecastday[0].astro.sunrise);
+              console.log("Sunset " + data.forecast.forecastday[0].astro.sunset);
+              console.log("humidity " + data.forecast.forecastday[0].day.avghumidity);
+              console.log("chances of rain " + data.forecast.forecastday[0].day.daily_chance_of_rain);
+              console.log("By the hour conditions ");
+              console.log("Chance of rain " + data.forecast.forecastday[0].hour[0].chance_of_rain);
+              console.log("Cloud " + data.forecast.forecastday[0].hour[0].cloud);
+              console.log("Sky condition " + data.forecast.forecastday[0].hour[0].condition.text);              // console.log("day 2 ");
+              // console.log(data.forecast.forecastday[1]);
+              // console.log("day 3 ");
+              // console.log(data.forecast.forecastday[2]);
+          });
+        } else {
+          console.log("error");
+        }
+      })
+
+}
+
+getWeather(); 
+
+    //pull from MapBox API for latitude and longitude
+const geocode = async()=>{
+    const response = await fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY3B0cGxhbmV0IiwiYSI6ImNreWFiNXA5OTAzcXkydnA5NWs1NXY1OWwifQ.jMJiAvDc9I0KPpUfg18U8g')
+    if(response.status === 200){
+      const data = await response.json()
+      console.log(data.features[0].center[0])
+    }
+    
+  }
+  geocode()
+  
+  // let address
+  // const geocode = async(address)=>{
+  //   const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiY3B0cGxhbmV0IiwiYSI6ImNreWFiNXA5OTAzcXkydnA5NWs1NXY1OWwifQ.jMJiAvDc9I0KPpUfg18U8g`)
+  //   if(response.status === 200){
+  //     const data = await response.json()
+  //     console.log(data.features[0].center[0])
+  //   }
+    
+  // }
+  // geocode(address)
+  
+  let locationSaved = []
+  
+  const createdLocation = (input)=>{
+      locationSaved.push(input)
+      saveLocation()
+  }
+  
+  
+  const saveLocation = ()=>{
+    localStorage.setItem('location', JSON.stringify(locationSaved))
+  }
+  
+  
+  const loadLocation = ()=>{
+    const locationJSON = locationStorage.getItem('locationSaved')
+  
+    try{
+      locationSaved = locationJSON ? JSON.parse(locationJSON) : []
+    }catch (error){
+      locationSaved = []
+    }
+  }
+  
+  
+  const generateSavedLocation = (location)=>{
+    const locationEl = document.createElement('label')
+  
+    const locationText = document.createElement('span')
+    locationText.textContent = location.textContent
+    locationEl.appendChild(locationText)
+ 
+  }
+
+
+  
+var locationInput = $('#location')[0];
+
+var locationName = "";
+
+// redirect from home page to results page
+function redirect(event) {
+  event.preventDefault();
+
+  // check that user inputted location
+  if (locationInput.value) {
+    locationName = locationInput.value;
+    //console.log(locationName);
+    location.replace("results.html");
+  } 
+}
+
+$('#submit').on('click', redirect);
 
 
 
