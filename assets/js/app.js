85491246f4569502ad72a3a33c7ff01caab2a703
Steven Schmidt <stevenschmidtaustex@gmail.com>
2022-01-12 13:58:45 -0600

    Additional Planetary Variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,40 +40,42 @@ function getAstro() {
         // Planet Position Variables
         // PlanetX = X Coordinates     PlanetY = Y Coordinates     PlanetRise = 'dawn' of that planet based on input'
         // PlanetM = Planet Magnitude: How bright the object is on a -(bright) to +(dull) scale. Sun is -27, naked eye limit is +6.5.
-        var mercuryX = data.data.table.rows[2].cells[0].position.horizonal.azimuth
-        var mercuryY = data.data.table.rows[2].cells[0].position.horizonal.altitude
+        var lunaX = data.data.table.rows[1].cells[0].position.horizonal.azimuth.degrees
+        var lunaY = data.data.table.rows[1].cells[0].position.horizonal.altitude.degrees
+        var lunaRise = data.data.table.rows[1].cells[0].position.equatorial.rightAscension.hours
+        var lunaM = data.data.table.rows[1].cells[0].extraInfo.magnitude
+        var mercuryX = data.data.table.rows[2].cells[0].position.horizonal.azimuth.degrees
+        var mercuryY = data.data.table.rows[2].cells[0].position.horizonal.altitude.degrees
         var mercuryRise = data.data.table.rows[2].cells[0].position.equatorial.rightAscension.hours
         var mercuryM = data.data.table.rows[2].cells[0].extraInfo.magnitude
-        var venusX = data.data.table.rows[3].cells[0].position.horizonal.azimuth
-        var venusY = data.data.table.rows[3].cells[0].position.horizonal.altitude
+        var venusX = data.data.table.rows[3].cells[0].position.horizonal.azimuth.degrees
+        var venusY = data.data.table.rows[3].cells[0].position.horizonal.altitude.degrees
         var venusRise = data.data.table.rows[3].cells[0].position.equatorial.rightAscension.hours
         var venusM = data.data.table.rows[3].cells[0].extraInfo.magnitude
-        var marsX = data.data.table.rows[5].cells[0].position.horizonal.azimuth
-        var marsY = data.data.table.rows[5].cells[0].position.horizonal.altitude
+        var marsX = data.data.table.rows[5].cells[0].position.horizonal.azimuth.degrees
+        var marsY = data.data.table.rows[5].cells[0].position.horizonal.altitude.degrees
         var marsRise = data.data.table.rows[5].cells[0].position.equatorial.rightAscension.hours
         var marsM = data.data.table.rows[5].cells[0].extraInfo.magnitude
-        var jupiterX = data.data.table.rows[6].cells[0].position.horizonal.azimuth
-        var jupiterY = data.data.table.rows[6].cells[0].position.horizonal.altitude
+        var jupiterX = data.data.table.rows[6].cells[0].position.horizonal.azimuth.degrees
+        var jupiterY = data.data.table.rows[6].cells[0].position.horizonal.altitude.degrees
         var jupiterRise = data.data.table.rows[6].cells[0].position.equatorial.rightAscension.hours
         var jupiterM = data.data.table.rows[6].cells[0].extraInfo.magnitude
-        var saturnX = data.data.table.rows[7].cells[0].position.horizonal.azimuth
-        var saturnY = data.data.table.rows[7].cells[0].position.horizonal.altitude
+        var saturnX = data.data.table.rows[7].cells[0].position.horizonal.azimuth.degrees
+        var saturnY = data.data.table.rows[7].cells[0].position.horizonal.altitude.degrees
         var saturnRise = data.data.table.rows[7].cells[0].position.equatorial.rightAscension.hours
         var saturnM = data.data.table.rows[7].cells[0].extraInfo.magnitude
-        var neptuneX = data.data.table.rows[8].cells[0].position.horizonal.azimuth
-        var neptuneY = data.data.table.rows[8].cells[0].position.horizonal.altitude
+        var neptuneX = data.data.table.rows[8].cells[0].position.horizonal.azimuth.degrees
+        var neptuneY = data.data.table.rows[8].cells[0].position.horizonal.altitude.degrees
         var neptuneRise = data.data.table.rows[8].cells[0].position.equatorial.rightAscension.hours
         var neptuneM = data.data.table.rows[8].cells[0].extraInfo.magnitude
-        var uranusX = data.data.table.rows[9].cells[0].position.horizonal.azimuth
-        var uranusY = data.data.table.rows[9].cells[0].position.horizonal.altitude
+        var uranusX = data.data.table.rows[9].cells[0].position.horizonal.azimuth.degrees
+        var uranusY = data.data.table.rows[9].cells[0].position.horizonal.altitude.degrees
         var uranusRise = data.data.table.rows[9].cells[0].position.equatorial.rightAscension.hours
         var uranusM = data.data.table.rows[9].cells[0].extraInfo.magnitude
-        var plutoX = data.data.table.rows[10].cells[0].position.horizonal.azimuth
-        var plutoY = data.data.table.rows[10].cells[0].position.horizonal.altitude
+        var plutoX = data.data.table.rows[10].cells[0].position.horizonal.azimuth.degrees
+        var plutoY = data.data.table.rows[10].cells[0].position.horizonal.altitude.degrees
         var plutoRise = data.data.table.rows[10].cells[0].position.equatorial.rightAscension.hours
         var plutoM = data.data.table.rows[10].cells[0].extraInfo.magnitude
-
-        console.log(mercuryX);
         });
         
 }
