154a410eb66aaefc835b203e28a6737344d8e866
Steven Schmidt <stevenschmidtaustex@gmail.com>
2022-01-12 13:16:08 -0600

    Added planetary locations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,72 +1,93 @@
-// const applicationSecret = '6f14831c8a735ba5d7c78419de6f4bd9a270586412858868719ccdb6c3ddf7f6f70d3f443c9b23b35acecacdadd9f74cefc634489ee774930c3150d63b1116b8ffd176c694ec0c8c9288fb86cd4c9fa84c3a7cf78d1f501292fe3eb8113b32dc19816d4f77e35c4977b00a527d9bc829'
-// const applicationId = '1ef02872-a5fd-4790-bebe-b572308c9bb6'
-// const hash = btoa(`${applicationId}:${applicationSecret}`);
+const astroApiSecret = '6f14831c8a735ba5d7c78419de6f4bd9a270586412858868719ccdb6c3ddf7f6f70d3f443c9b23b35acecacdadd9f74cefc634489ee774930c3150d63b1116b8ffd176c694ec0c8c9288fb86cd4c9fa84c3a7cf78d1f501292fe3eb8113b32dc19816d4f77e35c4977b00a527d9bc829'
+const astroApiId = '1ef02872-a5fd-4790-bebe-b572308c9bb6'
+const hash = btoa(`${astroApiId}:${astroApiSecret}`);
 
-// const getAstro = async() =>{
-//     const response = await fetch('https://salty-mountain-68764.herokuapp.com/https://api.astronomyapi.com/api/v2/bodies', {
-//     headers: new Headers(
-//       {
-//         "X-Requested-With": "XMLHttpRequest",
-//         Authorization: `Basic ${hash}`
-//     })
-// })
-// if(response.status===200){
-//         const data = await response.json()
-//         console.log(data)
-// }
-// }
+var requestUrl = new URL("https://api.astronomyapi.com/api/v2/bodies/positions")
 
-// getAstro()
+var params = {
+    longitude: "-84.39733",
+    latitude: "33.775867",
+    elevation: "50",
+    from_date: "2022-01-12",
+    to_date: "2022-01-13",
+    time: "20:00:00",
+  }
+ Object.keys(params).forEach(key => requestUrl.searchParams.append(key, params[key]))
 
-//** start of get weather function
-var weatherAPIKEY =  '27bbc4e6b84a47d1b13160933221101' ;
-var zipcodeNUM = '78634' ;
+function getAstro() {
+    fetch( 'https://salty-mountain-68764.herokuapp.com/' + requestUrl ,
+{       
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+                Authorization: `Basic ${btoa(
+                    `${astroApiId}:${astroApiSecret}`
+                )}`,
+            }
+        })
+        .then(function (response) {
+            console.log("outer response", response);
+            if (response.ok) {
+                console.log("response",response);
+            }
+            return response.json()
+        })
+        .then(function (data) {
+        console.log(data.data.table.rows)
+        console.log(data.data.table.rows[2].cells[0].position.equatorial)
+        console.log(data.data.table.rows[2].cells[0].position.equatorial.rightAscension)
+        console.log(data.data.table.rows[2].cells[0].position.equatorial.rightAscension.hours)
+        console.log(data.data.table.rows[2].cells[0].position.equatorial.rightAscension.string)
+        // Planet Position Variables
+        // PlanetX = X Coordinates     PlanetY = Y Coordinates     PlanetRise = 'dawn' of that planet based on input'
+        // PlanetM = Planet Magnitude: How bright the object is on a -(bright) to +(dull) scale. Sun is -27, naked eye limit is +6.5.
+        var mercuryX = data.data.table.rows[2].cells[0].position.horizonal.azimuth
+        var mercuryY = data.data.table.rows[2].cells[0].position.horizonal.altitude
+        var mercuryRise = data.data.table.rows[2].cells[0].position.equatorial.rightAscension.hours
+        var mercuryM = data.data.table.rows[2].cells[0].extraInfo.magnitude
+        var venusX = data.data.table.rows[3].cells[0].position.horizonal.azimuth
+        var venusY = data.data.table.rows[3].cells[0].position.horizonal.altitude
+        var venusRise = data.data.table.rows[3].cells[0].position.equatorial.rightAscension.hours
+        var venusM = data.data.table.rows[3].cells[0].extraInfo.magnitude
+        var marsX = data.data.table.rows[5].cells[0].position.horizonal.azimuth
+        var marsY = data.data.table.rows[5].cells[0].position.horizonal.altitude
+        var marsRise = data.data.table.rows[5].cells[0].position.equatorial.rightAscension.hours
+        var marsM = data.data.table.rows[5].cells[0].extraInfo.magnitude
+        var jupiterX = data.data.table.rows[6].cells[0].position.horizonal.azimuth
+        var jupiterY = data.data.table.rows[6].cells[0].position.horizonal.altitude
+        var jupiterRise = data.data.table.rows[6].cells[0].position.equatorial.rightAscension.hours
+        var jupiterM = data.data.table.rows[6].cells[0].extraInfo.magnitude
+        var saturnX = data.data.table.rows[7].cells[0].position.horizonal.azimuth
+        var saturnY = data.data.table.rows[7].cells[0].position.horizonal.altitude
+        var saturnRise = data.data.table.rows[7].cells[0].position.equatorial.rightAscension.hours
+        var saturnM = data.data.table.rows[7].cells[0].extraInfo.magnitude
+        var neptuneX = data.data.table.rows[8].cells[0].position.horizonal.azimuth
+        var neptuneY = data.data.table.rows[8].cells[0].position.horizonal.altitude
+        var neptuneRise = data.data.table.rows[8].cells[0].position.equatorial.rightAscension.hours
+        var neptuneM = data.data.table.rows[8].cells[0].extraInfo.magnitude
+        var uranusX = data.data.table.rows[9].cells[0].position.horizonal.azimuth
+        var uranusY = data.data.table.rows[9].cells[0].position.horizonal.altitude
+        var uranusRise = data.data.table.rows[9].cells[0].position.equatorial.rightAscension.hours
+        var uranusM = data.data.table.rows[9].cells[0].extraInfo.magnitude
+        var plutoX = data.data.table.rows[10].cells[0].position.horizonal.azimuth
+        var plutoY = data.data.table.rows[10].cells[0].position.horizonal.altitude
+        var plutoRise = data.data.table.rows[10].cells[0].position.equatorial.rightAscension.hours
+        var plutoM = data.data.table.rows[10].cells[0].extraInfo.magnitude
 
-// Get weather function 
-// will grab weather information from API 
-// data responds in current, forecast , location
-
-console.log("start Get Weather funtion");
-
-function getWeather () {
-    fetch('http://api.weatherapi.com/v1/forecast.json?key=' + weatherAPIKEY + '&q=' + zipcodeNUM + '&days=3')
-    
-      .then(function(response){
-        if (response.ok){
-
-          console.log("response ");
-          console.log(response);
-
-          response.json()
-            .then(function(data) {
-
-              console.log("data ");
-              console.log(data);
-
-              console.log("data current conditions ");
-              console.log(data.current.condition);
-              console.log("day 1 ");
-              console.log(data.forecast.forecastday[0]);
-              console.log("Moon Phase " + data.forecast.forecastday[0].astro.moon_phase);
-              console.log("Moonrise "+ data.forecast.forecastday[0].astro.moonrise);
-              console.log("Moonset " + data.forecast.forecastday[0].astro.moonset);
-              console.log("Sunrise " + data.forecast.forecastday[0].astro.sunrise);
-              console.log("Sunset " + data.forecast.forecastday[0].astro.sunset);
-              console.log("humidity " + data.forecast.forecastday[0].day.avghumidity);
-              console.log("chances of rain " + data.forecast.forecastday[0].day.daily_chance_of_rain);
-              console.log("By the hour conditions ");
-              console.log("Chance of rain " + data.forecast.forecastday[0].hour[0].chance_of_rain);
-              console.log("Cloud " + data.forecast.forecastday[0].hour[0].cloud);
-              console.log("Sky condition " + data.forecast.forecastday[0].hour[0].condition.text);              // console.log("day 2 ");
-              // console.log(data.forecast.forecastday[1]);
-              // console.log("day 3 ");
-              // console.log(data.forecast.forecastday[2]);
-          });
-        } else {
-          console.log("error");
-        }
-      })
+        console.log(mercuryX);
+        });
+        
 }
 
-getWeather(); 
+
+
+
+
+
+getAstro()
+
+
+
+
+
+
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,7 @@ function getAstro() {
 
 }
 
+var Mercury
+
 
 getAstro()
-
